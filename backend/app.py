94b6878d616da259694dffb8b241daaa89cf9cf0
Bearach <bearachbyrne@gmail.com>
2024-11-26 13:07:19 +0000

    Update data file references after moving data sets to backend

--- backend/app.py
+++ backend/app.py
@@ -4,15 +4,19 @@ from plot_rail_stops import plot_rail_stops, load_rail_stops
 
 app = Flask(__name__)
 
+# Base directory of the application
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # Serve the map
 @app.route('/')
 def index():
     # Paths to the GeoJSON files for rail stops
-    green_file_path = "./data/luas_stops_green.geojson"
-    red_file_path = "./data/luas_stops_red.geojson"
-    dart_file_path = "./data/dart_stations.geojson"
-    intercity_file_path = "./data/intercity_rail_stations.geojson"
-    commuter_file_path = "./data/dublin_commuter_stations.geojson"
+    green_file_path = os.path.join(BASE_DIR, "data", "luas_stops_green.geojson")
+    red_file_path = os.path.join(BASE_DIR, "data", "luas_stops_red.geojson")
+    dart_file_path = os.path.join(BASE_DIR, "data", "dart_stations.geojson")
+    intercity_file_path = os.path.join(BASE_DIR, "data", "intercity_rail_stations.geojson")
+    commuter_file_path = os.path.join(BASE_DIR, "data", "dublin_commuter_stations.geojson")
+
 
     # Load the rail stops data
     green_stops = load_rail_stops(green_file_path)
